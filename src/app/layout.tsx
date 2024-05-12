3aaf36e2112f6d1733f29f1f3b91bab673f4524a
josejooj <josejooj.dev@gmail.com>
2024-05-12 09:57:47 -0300

    feat: create footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Square Cloud Blob",
-  description: "A website created to simplify your experience with blob."
+  description: "A website created to simplify your experience with Square Cloud Blob."
 } satisfies Metadata;
 
 export default function RootLayout({ children }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.className + " dark:bg-black dark:text-gray-200 h-screen flex justify-center"}>
-        <main className="w-full max-w-[1200px] p-2">
+    <html lang="en">
+      <body className={inter.className + " dark:bg-black dark:text-gray-200 min-h-screen flex flex-col items-center"}>
+        <main className="flex-1 w-full max-w-[1200px] p-2">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import { IoMdInformationCircleOutline } from "react-icons/io";
+
+export default function Footer() {
+    return (
+        <footer className="flex gap-2 w-full items-center justify-center px-1 py-2 text-xs text-gray-300">
+            <IoMdInformationCircleOutline size={18} /> Note that this isn't an official website from Square Cloud.
+        </footer>
+    )
+}
