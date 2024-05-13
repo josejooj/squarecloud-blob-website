06c81a53d930029e7646d913274e1b03048cc672
josejooj <josejooj.dev@gmail.com>
2024-05-13 09:05:24 -0300

    feat: fetch object list on provider

--- src/components/files/provider.tsx
+++ src/components/files/provider.tsx
@@ -1,12 +1,37 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 // Create a Context
-const MyContext = createContext<FileContext>({ files: null, setFiles: () => { } });
+const MyContext = createContext<FileContext>({ files: [{ name: "teste", created_at: new Date(), size: 1024 }], setFiles: () => { } });
 
 // Create a Provider component
 export function FileProvider({ children }: { children: React.ReactNode }) {
 
     const [files, setFiles] = useState<File[] | null>(null);
+
+    useEffect(() => {
+
+        const fetcher = async () => {
+
+            const response = await fetch("https://blob.squarecloud.app/v1/list", {
+                headers: { Authorization: Cookies.get("apikey")! }
+            }).catch(e => e);
+
+            const data = await response.json?.().catch(() => { }) || {};
+
+            if (response.status !== 200 || !data.response.objects) return;
+
+            for (const object of data.response.objects) {
+                object.created_at = new Date(object.created_at)
+            }
+
+            setFiles(data.response.objects);
+
+        }
+
+        fetcher();
+
+    }, [])
 
     return (
         <MyContext.Provider value={{ files, setFiles }}>
