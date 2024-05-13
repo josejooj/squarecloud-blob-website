84c3d2e1eaaa5f00f3e44f0c1bc653620b690c4a
josejooj <josejooj.dev@gmail.com>
2024-05-13 09:08:16 -0300

    feat: list files show base data

--- src/components/files/list.tsx
+++ src/components/files/list.tsx
@@ -1,6 +1,9 @@
 'use client';
+import { ColumnDef } from "@tanstack/react-table";
+import DataTable from "../data-table";
 import { Label } from "../ui/label";
-import { useFileContext } from "./provider";
+import { File, useFileContext } from "./provider";
+import prettyBytes from "pretty-bytes";
 
 export default function ListFiles() {
 
@@ -12,7 +15,74 @@ export default function ListFiles() {
         </Label>
     )
 
+    const columns: ColumnDef<File>[] = [
+        {
+            header: "File name",
+            accessorKey: "name",
+            cell: ({ getValue }) => {
+
+                const value = getValue<string>();
+
+                return value.split("/").slice(1).join("/")
+
+            }
+        },
+        {
+            header: "Created at",
+            accessorKey: "created_at",
+            cell: ({ getValue }) => {
+
+                const value = getValue<Date>();
+                const DateFormatter = Intl.DateTimeFormat(navigator.language, {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "2-digit",
+                    minute: "2-digit",
+                    hour: "2-digit"
+                });
+
+                return (
+                    <span className="font-medium">{DateFormatter.format(value)}</span>
+                )
+
+            }
+        },
+        {
+            header: "Size",
+            accessorKey: "size",
+            cell: ({ getValue }) => {
+
+                const value = getValue<number>();
+                const minFileSize = 1000; // 1KB
+                const maxFileSize = 100000000; // 100MB
+
+                const minColor = [0, 255, 0]; // Green in RGB
+                const maxColor = [255, 0, 0]; // Red in RG
+                const normalizedValue = (value - minFileSize) / (maxFileSize - minFileSize);
+
+                const interpolatedColor = minColor.map((min, i) => {
+                    const max = maxColor[i];
+                    const component = Math.round(min + (max - min) * normalizedValue);
+                    return component.toString(16).padStart(2, '0');
+                });
+
+                const color = `#${interpolatedColor.join('')}`;
+
+                return (
+                    <span className="font-medium" style={{ color }}>{prettyBytes(value)}</span>
+                )
+
+            }
+        },
+        {
+            header: "Action",
+            cell: ({ getValue }) => {
+
+            }
+        }
+    ]
+
     return (
-        <div />
+        <DataTable columns={columns} data={files} />
     )
 }
