f8d8271ea899598626012636985fc575b340c6dc
josejooj <josejooj.dev@gmail.com>
2024-05-15 21:43:17 -0300

    enable pagination

--- src/components/files/set_page.tsx
+++ src/components/files/set_page.tsx
@@ -16,6 +16,23 @@ export default function SetPage({ table }: { table: Table<File> }) {
             >
                 <MdKeyboardArrowLeft size={24} />
             </Button>
+            <div className="text-sm">
+                <input
+                    className="h-full bg-transparent focus:outline-none cursor-default"
+                    style={{ width: `${(table.getState().pagination.pageIndex + 1).toString().length + 1}ch` }}
+                    value={table.getState().pagination.pageIndex + 1}
+                    onChange={e => {
+
+                        const page = +e.target.value.split("").filter(char => +char || char === "0").join("")
+
+                        if (!page || page >= table.getPageCount() + 1 || page <= 0) return;
+
+                        table.setPageIndex(page - 1);
+
+                    }}
+                />
+                / {table.getPageCount()}
+            </div>
             <Button
                 variant={'ghost'}
                 className="px-2 border-2"
