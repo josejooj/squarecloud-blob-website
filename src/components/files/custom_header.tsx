8ef554243cd520f57ff248ce5b71384f0b223efb
josejooj <josejooj.dev@gmail.com>
2024-05-15 22:01:32 -0300

    feat: create search field

--- src/components/files/custom_header.tsx
+++ src/components/files/custom_header.tsx
@@ -3,11 +3,13 @@ import AddFile from "./add";
 import DeleteFiles from "./delete";
 import SetPage from "./set_page";
 import { File } from "./provider";
+import Search from "./search";
 
 export default function CustomHeader({ table }: { table: Table<File> }) {
 
     return (
         <nav className="flex items-center gap-2">
+            <Search table={table}/>
             <AddFile />
             <DeleteFiles />
             <SetPage table={table} />
---
+++ src/components/files/search.tsx
@@ -0,0 +1,17 @@
+import { Table } from "@tanstack/react-table";
+import { File } from "./provider";
+import { Input } from "../ui/input";
+
+export default function Search({ table }: { table: Table<File> }) {
+
+    return (
+        <Input
+            placeholder="Search by file name"
+            className="w-full"
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+                table.getColumn("name")?.setFilterValue(event.target.value)
+            }
+        />
+    )
+}
