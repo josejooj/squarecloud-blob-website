03b360ddb7fa9ce482a3385c23c8c0c766c3043a
josejooj <josejooj.dev@gmail.com>
2024-06-14 22:26:37 -0300

    fix: header as server component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,27 @@
-'use client';
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import ThemeModeSwitch from "./themeModeSwitch";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
-export default function Footer() {
-
-    const [mounted, setMounted] = useState(false);
-    const { theme } = useTheme();
-
-    useEffect(() => { setMounted(true) }, [])
+export default function Header() {
 
     return (
         <header className="flex gap-2 w-full max-w-[1200px] items-center justify-between px-1 py-2">
             <Link href="https://squarecloud.app" className="flex items-center gap-2">
-                <Image src={`/logo-${mounted && theme === 'dark' ? "white" : 'black'}-nobg.png`} width={64} height={64} alt="Square Cloud Logo" />
+                <Image
+                    src={`/logo-dark-nobg.png`}
+                    width={64}
+                    height={64}
+                    alt="Square Cloud Logo"
+                    className="dark:hidden"
+                />
+                <Image
+                    src="/logo-white-nobg.png"
+                    width={64}
+                    height={64}
+                    alt="Square Cloud Logo"
+                    className="hidden dark:block"
+                />
                 <h1 className="text-2xl font-bold">Blob</h1>
             </Link>
             <article className="flex gap-4 items-center">
