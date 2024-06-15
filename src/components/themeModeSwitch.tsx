8a8fc6ac888eac6edb4f9a046a2d9c9d53595bdb
josejooj <josejooj.dev@gmail.com>
2024-06-14 22:29:52 -0300

    perf: remove invalid hooks

--- src/components/themeModeSwitch.tsx
+++ src/components/themeModeSwitch.tsx
@@ -1,18 +1,10 @@
 'use client';
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 import { CiCloudMoon, CiCloudSun } from "react-icons/ci";
 
 export default function ThemeModeSwitch() {
 
-    const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme()
-    const IconTheme = theme === 'dark' ? CiCloudSun : CiCloudMoon;
-
-    useEffect(() => { setMounted(true) }, [])
-
-    if (!mounted) return null;
-
     const handleThemeChange = () => {
         if (theme === 'light') return setTheme("dark");
         else return setTheme("light");
@@ -20,7 +12,8 @@ export default function ThemeModeSwitch() {
 
     return (
         <div onClick={handleThemeChange} className="cursor-pointer">
-            <IconTheme size={32} />
+            <CiCloudSun size={32} className="dark:hidden" />
+            <CiCloudMoon size={32} className="hidden dark:block"/>
         </div>
     )
 
