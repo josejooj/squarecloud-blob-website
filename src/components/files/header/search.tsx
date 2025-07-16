6ce7ad537f6641fb7a2e6c755f15947177255684
josejooj <josejooj.dev@gmail.com>
2025-07-16 11:36:53 -0300

    refactor: autofocus on search

--- src/components/files/header/search.tsx
+++ src/components/files/header/search.tsx
@@ -8,6 +8,7 @@ export default function Search({ table }: { table: Table<File> }) {
         <div className="flex items-center gap-2 w-full border rounded-md px-2">
             <IoMdSearch size={24} className=""/>
             <input
+                autoFocus
                 placeholder="Search by ID"
                 className="w-full py-2 text-sm bg-transparent focus:outline-none"
                 value={(table.getColumn("id")?.getFilterValue() as string) ?? ""}
